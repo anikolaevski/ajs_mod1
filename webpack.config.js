c51342c7c768d5736d8eacee960d71512612fafa
anikolaevski <anikolaevski@gmail.com>
2019-05-20 22:26:30 +0300

    v0.2.1 MiniCssExtractPlugin does not add css

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,27 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
   entry: './src/index.js',
   output: {
     path: __dirname + '/dist',
     filename: 'main.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+        MiniCssExtractPlugin.loader, 'css-loader',
+        ],
+      },
+    ]
+  },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+      }),
     new HtmlWebpackPlugin(
       {
         // title: 'My App',
